1ffa35a662d1a37cb07680776551778b3148ad96
Patrick Sullivan <psullivan6@gmail.com>
2020-10-02 16:57:15 -0400

    Preview Additional Info

---
+++ src/components/FormPage/index.jsx
@@ -0,0 +1,167 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Field, Form, Formik } from "formik";
+import { v4 as uuidv4 } from "uuid";
+
+// Utilities
+import { useAppContext } from "../../utilities/AppContext";
+import { initialFormValues } from "../../utilities/constants";
+
+// Components
+import DatePicker from "../DatePicker";
+import Table from "../Table";
+
+const FormPage = () => {
+  const { storiesObj: stories, setStories } = useAppContext();
+  const [crudStatus, setCrudStatus] = useState("create");
+  const [hasUnsavedData, setHasUnsavedData] = useState(false);
+
+  const handleBeforeUnload = useCallback(
+    (event) => {
+      if (hasUnsavedData) {
+        event.returnValue =
+          "You haven't downloaded the updated JSON. Are you sure you want to leave?";
+      }
+    },
+    [hasUnsavedData]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("stories", JSON.stringify(stories));
+  }, [stories]);
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [handleBeforeUnload]);
+
+  const handleSubmit = (values, actions) => {
+    const id = crudStatus === "create" ? uuidv4() : values.id;
+
+    actions.setSubmitting(false);
+    actions.resetForm({
+      values: initialFormValues
+    });
+
+    setStories({
+      ...stories,
+      [id]: {
+        ...values,
+        id
+      }
+    });
+
+    if (crudStatus === "update") {
+      setCrudStatus("create");
+    }
+
+    if (!hasUnsavedData) {
+      setHasUnsavedData(true);
+    }
+  };
+
+  const handleEditItem = ({ data }) => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+    setCrudStatus("update");
+  };
+
+  const handleRemoveItem = ({ id }) => {
+    const { [id]: removedItem, ...rest } = stories;
+    setStories(rest);
+  };
+
+  const handleDownload = () => {
+    setHasUnsavedData(false);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h2>
+          Fill out the fields below, click the "Download JSON" button and share
+          the file with the Engineering team.
+        </h2>
+      </header>
+      <Formik initialValues={initialFormValues} onSubmit={handleSubmit}>
+        <Form>
+          <label htmlFor="title">Title:</label>
+          <Field id="title" name="title" placeholder="Title" />
+
+          <div className="DateBox">
+            <div>
+              <label htmlFor="publishDate">Publish Date:</label>
+              {/* Saved as ISO string */}
+              <DatePicker
+                id="publishDate"
+                name="publishDate"
+                placeholderText="Click to select a date"
+                showTimeSelect
+                dateFormat="MM/dd/yyyy h:mm aa"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="expirationDate">Expiration Date:</label>
+              {/* Saved as ISO string */}
+              <DatePicker
+                id="expirationDate"
+                name="expirationDate"
+                placeholderText="Leave Blank if none"
+                showTimeSelect
+                dateFormat="MM/dd/yyyy h:mm aa"
+              />
+            </div>
+          </div>
+
+          <br />
+
+          <label htmlFor="url">Link URL:</label>
+          <Field id="url" name="url" placeholder="Link URL" />
+
+          <br />
+
+          <label htmlFor="description">Description:</label>
+          <Field
+            id="description"
+            as="textarea"
+            name="description"
+            placeholder="Description"
+          />
+
+          <button type="submit" className={`Button-${crudStatus}`}>
+            {`${crudStatus === "create" ? "Add" : "Update"} Story`}
+          </button>
+
+          <br />
+          <br />
+
+          <Table
+            data={stories}
+            onEditItem={handleEditItem}
+            onRemoveItem={handleRemoveItem}
+          />
+          <br />
+
+          <a
+            className="Button"
+            download="stories.json"
+            href={`data:text/json;charset=utf-8,${encodeURIComponent(
+              JSON.stringify(Object.values(stories))
+            )}`}
+            onClick={handleDownload}
+          >
+            Download JSON
+          </a>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default FormPage;
--- src/components/PreviewPage/index.jsx
+++ src/components/PreviewPage/index.jsx
@@ -2,20 +2,47 @@ import React from "react";
 import { format, parseISO } from "date-fns";
 import "./styles.css";
 import { useAppContext } from "../../utilities/AppContext";
+import { parseDate } from "../../utilities/date";
 
 const PreviewItem = ({ publishDate, title, description, url }) => {
-  const date =
-    typeof publishDate === "string" ? parseISO(publishDate) : publishDate;
-
   return (
     // eslint-disable-next-line react/jsx-no-target-blank
     <a className="PreviewItem" href={url} target="_blank">
       <header>
         <h1>{title}</h1>
-        <span>{format(date, "MM/dd/yy")}</span>
+        <span>{format(parseDate(publishDate), "MM/dd/yy")}</span>
       </header>
       <p>{description}</p>
     </a>
+  );
+};
+
+const AdditionalInfo = ({ expirationDate, ...props }) => {
+  const date =
+    typeof expirationDate === "string"
+      ? parseISO(expirationDate)
+      : expirationDate;
+
+  const failedKeys = Object.keys(props).filter((key) => !props[key]);
+
+  console.log("failedKeys", failedKeys);
+
+  if (!expirationDate || failedKeys.length === 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>Additional Info:</h1>
+      <ul>
+        {expirationDate && (
+          <li>{`Expiration Date: ${format(date, "MM/dd/yy")}`}</li>
+        )}
+        {failedKeys.map((failedKey) => (
+          <li>{`Oops, you forgot ${failedKey}`}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
@@ -26,21 +53,13 @@ const PreviewPage = () => {
   const activeStories = stories.filter((story) => {
     const publishUnix = new Date(story.publishDate).getTime();
 
-    if (publishUnix > now) {
-      return false;
-    }
-
-    return true;
+    return publishUnix <= now;
   });
 
   const futureStories = stories.filter((story) => {
     const publishUnix = new Date(story.publishDate).getTime();
 
-    if (publishUnix > now) {
-      return true;
-    }
-
-    return false;
+    return publishUnix > now;
   });
 
   return (
@@ -48,14 +67,20 @@ const PreviewPage = () => {
       <h1>Stories Preview</h1>
       <h2>Current Stories:</h2>
       {activeStories.map((story) => (
-        <PreviewItem key={story.id} {...story} />
+        <div className="PreviewContainer">
+          <PreviewItem key={story.id} {...story} />
+          <AdditionalInfo {...story} />
+        </div>
       ))}
       <br />
       <hr />
       <br />
       <h2>Future Stories:</h2>
       {futureStories.map((story) => (
-        <PreviewItem key={story.id} {...story} />
+        <div className="PreviewContainer">
+          <PreviewItem key={story.id} {...story} />
+          <AdditionalInfo {...story} />
+        </div>
       ))}
     </div>
   );
